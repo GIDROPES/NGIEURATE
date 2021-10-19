0c47f46b1484286c3a10b7a80faa0215375e6fd3
GIDROPES <gidropes2002@gmail.com>
2021-10-19 12:43:25 +0300

    ProfileActivity uploadDialog added

--- app/src/main/java/com/example/ngieurate/ProfileUser.java
+++ app/src/main/java/com/example/ngieurate/ProfileUser.java
@@ -1,14 +1,57 @@
 package com.example.ngieurate;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileUser extends AppCompatActivity {
-
+    ImageView clickToUpload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_user);
+        clickToUpload = (ImageView)  findViewById(R.id.clickToUpload);
+        clickToUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getSupportFragmentManager();
+                AskUserUploadDialog askDialog = new AskUserUploadDialog();
+                askDialog.show(manager, "askUserUploadDialog");
+            }
+        });
+    }
+    public static class AskUserUploadDialog extends AppCompatDialogFragment{
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Выберите способ загрузки")
+                    .setMessage("Сфотографируйте достижение или загрузите готовое фото")
+                    .setIcon(R.drawable.upload_image)
+                    .setNegativeButton("КАМЕРА", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .setPositiveButton("ГАЛЕРЕЯ", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+
+            return builder.create();
+        }
     }
 }
