a87d8d2090ece75786bae7dd5956761440261b74
GIDROPES <gidropes2002@gmail.com>
2021-12-04 23:38:07 +0300

    working ShowingTable but need to correct

--- app/src/main/java/com/example/ngieurate/ProfileUser.java
+++ app/src/main/java/com/example/ngieurate/ProfileUser.java
@@ -10,9 +10,11 @@ import androidx.fragment.app.FragmentManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,12 +26,13 @@ public class ProfileUser extends AppCompatActivity {
     String fio, grNum;
     Integer posAll, posGr, allPnts;
     SharedPreferences myData;
-
+    Button checkRateBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_user);
-        //сопоставляем кнопочки
+        //сопоставляем кнопочки и другие нажималки
+        checkRateBtn = findViewById(R.id.checkRateBtn);
         clickToUpload =  findViewById(R.id.clickToUpload);
         //сопоставляем текстовые штуки
         nameFio = findViewById(R.id.nameFio);
@@ -44,6 +47,13 @@ public class ProfileUser extends AppCompatActivity {
                 FragmentManager manager = getSupportFragmentManager();
                 AskUserUploadDialog askDialog = new AskUserUploadDialog();
                 askDialog.show(manager, "askUserUploadDialog");
+            }
+        });
+        checkRateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileUser.this, ShowTable.class);
+                startActivity(intent);
             }
         });
         myData = getSharedPreferences(LoginUser.APP_PREFERENCES, MODE_PRIVATE);
---
+++ app/src/main/java/com/example/ngieurate/ShowTable.java
@@ -0,0 +1,70 @@
+package com.example.ngieurate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShowTable extends AppCompatActivity {
+    Button backBtn;
+    GridView mainGridView;
+    SimpleAdapter adapter;
+    Connection connection;
+    SQLSenderConnector sender;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_table);
+        backBtn = findViewById(R.id.buttonBack);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowTable.this, ProfileUser.class);
+                startActivity(intent);
+            }
+        });
+        mainGridView =  findViewById(R.id.mainGrid);
+
+        List<Map<String,String>> data = new ArrayList<Map<String,String>>();
+        try{
+            sender = new SQLSenderConnector();
+            connection = sender.toOwnConnection();
+            if (connection!=null){
+                String query = "SELECT ROW_NUMBER() OVER (ORDER BY ALL_POINTS DESC) AS RATE, FIO,GROUP_NUMBER,ALL_POINTS FROM STUDENT_DATA;";
+                Statement statement = connection.createStatement();
+                ResultSet set = statement.executeQuery(query);
+                    while (set.next()){
+                        Map<String, String> tab = new HashMap<String, String>();
+                        tab.put("RATE", set.getString("RATE"));
+                        tab.put("FIO", set.getString("FIO"));
+                        tab.put("GROUP_NUMBER", set.getString("GROUP_NUMBER"));
+                        tab.put("ALL_POINTS", set.getString("ALL_POINTS"));
+                        data.add(tab);
+                    }
+                    String[] from = {"RATE","FIO","GROUP_NUMBER","ALL_POINTS"};
+                    int[] to = {R.id.rate,R.id.fioTable,R.id.grNumTable,R.id.pointsTable};
+                    adapter = new SimpleAdapter(ShowTable.this, data, R.layout.gridview_layout,from,to);
+                    mainGridView.setAdapter(adapter);
+            }
+        } catch (SQLException throwables) {
+            Log.d("SHOW ERROR: ", throwables.getMessage());
+            throwables.printStackTrace();
+        }
+
+    }
+}
